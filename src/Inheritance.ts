0e7ab98a0367d32da4a6e0d9eef44680af79433e
ilya <ilya140102@yandex.ru>
2022-04-10 22:18:29 +0300

    task-inheritance

--- src/Inheritance.ts
+++ src/Inheritance.ts
@@ -16,6 +16,20 @@ export class Person {
     readonly _name: string;
     readonly _secondName: string;
     readonly _age: number;
+
+    constructor(iPersonData: IPersonData) {
+        this._name = iPersonData.name;
+        this._secondName = iPersonData.secondName;
+        this._age = iPersonData.age;
+    }
+
+    getData(): IPersonData {
+        return {
+            name: this._name,
+            secondName: this._secondName,
+            age: this._age,
+        };
+    }
 }
 
 /*
@@ -24,4 +38,18 @@ export class Person {
  */
 export class Student extends Person {
     readonly _phone: string;
+
+    constructor(iStudentData: IStudentData) {
+        super(iStudentData);
+        this._phone = iStudentData.phone;
+    }
+
+    getData(): IStudentData {
+        return {
+            phone: this._phone,
+            name: this._name,
+            secondName: this._secondName,
+            age: this._age,
+        };
+    }
 }
